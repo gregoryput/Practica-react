f30cadfc78d117d72b8dc137ab0f83bf3499affc
gregoryput <gregory-sanchez7878@hotmail.com>
2023-03-15 15:56:26 -0400

    convertido a funcion

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import Componente_A from "./components/Componente_A";
-import Componente_B from "./components/container/Componente_B";
+
+import Clock from "./components/Clock";
+import NewClock from "./components/NewClock";
+
+ 
 
 function App() {
   return (
     <div className="App">
-     <Componente_A/>
-
+      <Clock/>
+     <NewClock/>
     </div>
   );
 }
